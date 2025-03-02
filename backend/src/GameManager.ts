c0e34a7b709c01c552b0ce1ddc5bdd0ad0ee17e7
harshsrivastava05 <harshsrivastava2316@gmail.com>
2025-03-02 22:19:56 +0530

    first commit

---
+++ backend/src/GameManager.ts
@@ -0,0 +1,53 @@
+import { WebSocket } from "ws";
+import { INIT_GAME, MOVE } from "./Messages";
+import { Game } from "./Game";
+
+export class GameManager {
+  private games: Game[];
+  private pendingUsers: WebSocket | null;
+  private users: WebSocket[];
+
+  constructor() {
+    this.games = [];
+    this.pendingUsers = null;
+    this.users = [];
+  }
+
+  addUser(socket: WebSocket) {
+    // Add user to the game
+    this.users.push(socket);
+    this.addHandler(socket);
+  }
+
+  removeUser(socket: WebSocket) {
+    this.users = this.users.filter((user) => user !== socket);
+    // stop the game as the user left
+  }
+
+  private addHandler(socket: WebSocket) {
+    socket.on("message", (data) => {
+      const message = JSON.parse(data.toString());
+
+      if (message.type === INIT_GAME) {
+        // ✅ Correct condition
+        if (this.pendingUsers) {
+          // Start the game
+          const game = new Game(this.pendingUsers, socket);
+          this.games.push(game);
+          this.pendingUsers = null;
+        } else {
+          this.pendingUsers = socket;
+        }
+      }
+
+      if (message.type === MOVE) {
+        const game = this.games.find(
+          (game) => game.player1 === socket || game.player2 === socket
+        );
+        if (game) {
+          game.makeMove(socket, message.move);
+        }
+      }
+    });
+  }
+}
